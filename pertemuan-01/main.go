583eb8e236f08816c18e640d546d4afe2e590ced
Fathiya <fathiyahaya8c@gmail.com>
2026-09-03 12:27:17 +0700

    Add Sapa to CetakInfo

--- pertemuan-01/main.go
+++ pertemuan-01/main.go
@@ -27,7 +27,7 @@ func Sapa(nama string) string {
 // TODO(Level 5): gabungkan Nama, NRP, dan hasil runtime.Version() jadi satu
 // string siap cetak (lihat contoh format di SOAL.md).
 func CetakInfo(nama string) string {
-	return fmt.Sprintf("Nama: %s \n NRP: %s \n %s", nama, NRP, runtime.Version())
+	return fmt.Sprintf("Nama: %s \n NRP: %s \n %s \n %s", nama, NRP, runtime.Version(), Sapa(nama))
 }
 
 func main() {
