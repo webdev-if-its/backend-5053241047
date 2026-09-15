3a63eeebbde02498c0af702904799c8ef9f55010
Fathiya <fathiyahaya8c@gmail.com>
2026-09-15 18:55:44 +0700

    pertemuan-03: level 9 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -103,7 +103,14 @@ func AmankanPanggilan(fn func() error) (err error) {
 // request, server tetap hidup untuk request-request lain (request yang
 // panic itu dijawab status 500).
 func AmankanHandler(next http.HandlerFunc) http.HandlerFunc {
-	panic("belum diimplementasikan")
+	return func (w http.ResponseWriter, r *http.Request)  {
+		defer func ()  {
+			if rec := recover(); rec != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+		next(w, r)
+	}
 }
 
 // RekapStatus menghitung berapa tugas yang sudah selesai dan berapa yang
