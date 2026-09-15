92c895f728d05de1433bf1b16e70300041ce071b
Fathiya <fathiyahaya8c@gmail.com>
2026-09-15 17:28:58 +0700

    pertemuan-03: level 2 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -48,7 +48,12 @@ func TambahTugas(toko *TokoTugas, judul string) (Task, error) {
 }
 
 func LihatTugas(toko *TokoTugas, id int) (Task, error) {
-	panic("belum diimplementasikan")
+	for _, tugas := range toko.Daftar {
+		if tugas.ID == id {
+			return tugas, nil
+		}
+	}
+	return Task{}, nil
 }
 
 func HapusTugas(toko *TokoTugas, id int) error {
