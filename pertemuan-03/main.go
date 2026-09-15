9134954cddbd4b54438b2622c4b36cd858505d3a
Fathiya <fathiyahaya8c@gmail.com>
2026-09-15 17:42:43 +0700

    pertemuan-03: level 5 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -63,7 +63,7 @@ func HapusTugas(toko *TokoTugas, id int) error {
 			return nil
 		}
 	}
-	return nil
+	return ErrTugasTidakDitemukan
 }
 
 // HapusTugasTercatat memanggil HapusTugas, lalu memakai defer untuk
