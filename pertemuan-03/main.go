430a7ea6b091fb23329f9af4dbec4c84886eec4d
Fathiya <fathiyahaya8c@gmail.com>
2026-09-15 17:41:02 +0700

    pertemuan-03: level 4 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -53,7 +53,7 @@ func LihatTugas(toko *TokoTugas, id int) (Task, error) {
 			return tugas, nil
 		}
 	}
-	return Task{}, nil
+	return Task{}, ErrTugasTidakDitemukan
 }
 
 func HapusTugas(toko *TokoTugas, id int) error {
